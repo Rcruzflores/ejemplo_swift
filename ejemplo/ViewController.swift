93cc700336fe67ff617f2093e29c5c43d263429f
rcruz <rcruz@oritec.cl>
2018-09-21 17:28:25 -0300

    Initial Commit

---
+++ ejemplo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ejemplo
+//
+//  Created by Rubén on 21-09-18.
+//  Copyright © 2018 Rubén. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
