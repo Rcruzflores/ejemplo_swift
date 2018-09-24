3f8319de1c90e71abd882b60c7b850938214328a
rcruz <rcruz@oritec.cl>
2018-09-24 10:42:08 -0300

    Ejemplo commit

--- ejemplo/ViewController.swift
+++ ejemplo/ViewController.swift
@@ -1,10 +1,4 @@
-//
-//  ViewController.swift
-//  ejemplo
-//
-//  Created by Rubén on 21-09-18.
-//  Copyright © 2018 Rubén. All rights reserved.
-//
+
 
 import UIKit
 
@@ -12,9 +6,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Ejemplo")
+        
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
